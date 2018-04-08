90d8e4f58e410136e8c3e234d6360d9d219e8666
admblack97 <admblack97@gmail.com>
2018-04-08 14:39:04 +0800

    instance, buat atribut dan panggil method

--- src/tugas3/aksi.java
+++ src/tugas3/aksi.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugas3;
 
-/**
- *
- * @author Adam Morseleno
- */
 public class aksi {
-    
+    public static void main(String[] args) {
+        hewan h = new hewan();
+        h.nama="kucing";
+        h.golongan="karnivora";
+        h.aktivitas="nokturnal";
+        
+        h.cetak();
+    }
 }
