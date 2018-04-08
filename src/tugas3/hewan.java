e8b0d62603805b33a4368f7b2d2f603c7fd168cd
admblack97 <admblack97@gmail.com>
2018-04-08 14:43:25 +0800

    membuat getter and setter

--- src/tugas3/hewan.java
+++ src/tugas3/hewan.java
@@ -1,15 +1,41 @@
 package tugas3;
 
 public class hewan {
-    String nama;
-    String golongan;
-    String aktivitas;
+    private String nama;
+    private String golongan;
+    private String aktivitas;
 
     void cetak(){
         System.out.println("Nama Hewan \t: "+nama+"\n"+
                            "Golongan \t: "+golongan+"\n"+
                            "Aktivitas \t: "+aktivitas);
     }
+
+    public String getnama() {
+        return nama;
+    }
+
+    public void setnama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getgolongan() {
+        return golongan;
+    }
+
+    public void setgolongan(String golongan) {
+        this.golongan = golongan;
+    }
+
+    public String getaktivitas() {
+        return aktivitas;
+    }
+
+    public void setaktivitas(String aktivitas) {
+        this.aktivitas = aktivitas;
+    }
+    
+    
 }
 
 
