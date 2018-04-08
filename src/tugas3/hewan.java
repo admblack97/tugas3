2600c2d5371fda2dd401e95ea07857a0fd5f3ea7
admblack97 <admblack97@gmail.com>
2018-04-08 14:14:13 +0800

    membuat method cetak

--- src/tugas3/hewan.java
+++ src/tugas3/hewan.java
@@ -4,4 +4,12 @@ public class hewan {
     String nama;
     String golongan;
     String aktivitas;
+
+    void cetak(){
+        System.out.println("Nama Hewan \t: "+nama+"\n"+
+                           "Golongan \t: "+golongan+"\n"+
+                           "Aktivitas \t: "+aktivitas);
+    }
 }
+
+
